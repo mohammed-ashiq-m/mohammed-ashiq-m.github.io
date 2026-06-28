2a04b04c17b1e69ec6eb0135b671ebc5f750aa18
Mohammed Ashiq <mohammed.ashiq@electronikmedia.com>
2026-06-28 11:46:03 +0530

    Set up Pages deployment

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";
 import { config } from "../config";
 import "./styles/CallToAction.css";
 
--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,12 +1,7 @@
 import { PropsWithChildren } from "react";
 import "./styles/Landing.css";
-import { config } from "../config";
 
 const Landing = ({ children }: PropsWithChildren) => {
-  const nameParts = config.developer.fullName.split(" ");
-  const firstName = nameParts[0] || config.developer.name;
-  const lastName = nameParts.slice(1).join(" ") || "";
-
   return (
     <>
       <div className="landing-section" id="landingDiv">
